3a6f250bf11d94b41662e1ad3c5867f53837a8f9
jmottershead94 <jasonmottershead@hotmail.co.uk>
2016-12-15 19:23:42 +0000

    Update - Added initial project files and setup SFML static libraries and includes.

---
+++ herberts_adventure/herberts_adventure/main.cpp
@@ -0,0 +1,22 @@
+/* Include header files here. */
+/* Application files. */
+
+/*
+
+	Overview
+	========
+	This will be the main execution point for the program. The main game
+	loop will be in this file.
+
+	Returns
+	=======
+	int value	-	The standard value returned for the main function.
+
+*/
+int main()
+{
+
+
+	/* Standard return for the main function. */
+	return 0;
+}
